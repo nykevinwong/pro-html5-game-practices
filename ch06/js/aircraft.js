2f7ad58f236c1e7e246028de4dfcde731bb95df0
nyckevinwong <nyckevinwong@gmail.com>
2019-02-11 23:12:01 -0500

    chapter 6 - added selection/lifebar drawing code

--- ch06/js/aircraft.js
+++ ch06/js/aircraft.js
@@ -75,16 +75,55 @@ var aircraft = {
                 break;
             }
         },
-        draw:function(){
-            var x = (this.x*game.gridSize)-game.offsetX-this.pixelOffsetX;
-            var y = (this.y*game.gridSize)-game.offsetY-this.pixelOffsetY-this.pixelShadowHeight;
-            var colorIndex = (this.team == "blue")?0:1;
-            var colorOffset = colorIndex*this.pixelHeight;
-            var shadowOffset = this.pixelHeight*2; // The aircraft shadow is on the second row of the sprite sheet
-     
-            game.foregroundContext.drawImage(this.spriteSheet, this.imageOffset*this.pixelWidth, colorOffset, this.pixelWidth, this.pixelHeight, x, y, this.pixelWidth,this.pixelHeight);
-            game.foregroundContext.drawImage(this.spriteSheet, this.imageOffset*this.pixelWidth, shadowOffset, this.pixelWidth, this.pixelHeight, x, y+this.pixelShadowHeight, this.pixelWidth, this.pixelHeight);
-        }
+		drawLifeBar:function(){
+			var x = this.drawingX;
+			var y = this.drawingY - 2*game.lifeBarHeight;
+			game.foregroundContext.fillStyle = (this.lifeCode ==
+			"healthy")?game.healthBarHealthyFillColor:game.healthBarDamagedFillColor;
+
+		game.foregroundContext.fillRect(x,y,this.pixelWidth*this.life/this.hitPoints,game.lifeBarHeight)
+			game.foregroundContext.strokeStyle = game.healthBarBorderColor;
+			game.foregroundContext.lineWidth = 1;
+			game.foregroundContext.strokeRect(x,y,this.pixelWidth,game.lifeBarHeight)
+		},
+		drawSelection:function(){
+			var x = this.drawingX + this.pixelOffsetX;
+			var y = this.drawingY + this.pixelOffsetY;
+			game.foregroundContext.strokeStyle = game.selectionBorderColor;
+			game.foregroundContext.lineWidth = 2;
+			game.foregroundContext.beginPath();
+			game.foregroundContext.arc(x,y,this.radius,0,Math.PI*2,false);
+			game.foregroundContext.stroke();
+			game.foregroundContext.fillStyle = game.selectionFillColor;
+			game.foregroundContext.fill();
+		  
+			game.foregroundContext.beginPath();
+			game.foregroundContext.arc(x,y+this.pixelShadowHeight,4,0,Math.PI*2,false);
+			game.foregroundContext.stroke();
+		  
+			game.foregroundContext.beginPath();
+			game.foregroundContext.moveTo(x,y);
+			game.foregroundContext.lineTo(x,y+this.pixelShadowHeight);
+			game.foregroundContext.stroke();
+		},
+		draw:function(){
+			var x = (this.x*game.gridSize)-game.offsetX-this.pixelOffsetX;
+			var y = (this.y*game.gridSize)-game.offsetY-this.pixelOffsetY-this.pixelShadowHeight;
+			this.drawingX = x;
+			this.drawingY = y;
+			if (this.selected){
+				this.drawSelection();
+				this.drawLifeBar();
+			}
+			var colorIndex = (this.team == "blue")?0:1;
+			var colorOffset = colorIndex*this.pixelHeight;
+			var shadowOffset = this.pixelHeight*2; // The aircraft shadow is on the second row of the sprite sheet
+		  
+			game.foregroundContext.drawImage(this.spriteSheet, this.imageOffset*this.pixelWidth,
+		colorOffset, this.pixelWidth, this.pixelHeight, x, y, this.pixelWidth, this.pixelHeight);
+			game.foregroundContext.drawImage(this.spriteSheet, this.imageOffset*this.pixelWidth,
+		shadowOffset, this.pixelWidth, this.pixelHeight, x, y+this.pixelShadowHeight, this.pixelWidth,this.pixelHeight);
+		}
     },
     load:loadItem,
     add:addItem,
--- ch06/js/vehicles.js
+++ ch06/js/vehicles.js
@@ -107,13 +107,42 @@ var vehicles = {
                 break;
             }
         },
-        draw:function(){
-            var x = (this.x*game.gridSize)-game.offsetX-this.pixelOffsetX;
-            var y = (this.y*game.gridSize)-game.offsetY-this.pixelOffsetY;
-            var colorIndex = (this.team == "blue")?0:1;
-            var colorOffset = colorIndex*this.pixelHeight;
-            game.foregroundContext.drawImage(this.spriteSheet, this.imageOffset*this.pixelWidth, colorOffset, this.pixelWidth, this.pixelHeight, x, y, this.pixelWidth, this.pixelHeight);
-        }
+		drawLifeBar:function(){
+			var x = this.drawingX;
+			var y = this.drawingY - 2*game.lifeBarHeight;
+			game.foregroundContext.fillStyle = (this.lifeCode == "healthy")?game.healthBarHealthyFillColor:game.healthBarDamagedFillColor;
+
+			game.foregroundContext.fillRect(x,y,this.pixelWidth*this.life/this.hitPoints,game.lifeBarHeight)
+			game.foregroundContext.strokeStyle = game.healthBarBorderColor;
+			game.foregroundContext.lineWidth = 1;
+			game.foregroundContext.strokeRect(x,y,this.pixelWidth,game.lifeBarHeight)
+		},
+		drawSelection:function(){
+			var x = this.drawingX + this.pixelOffsetX;
+			var y = this.drawingY + this.pixelOffsetY;
+			game.foregroundContext.strokeStyle = game.selectionBorderColor;
+			game.foregroundContext.lineWidth = 1;
+			game.foregroundContext.beginPath();
+			game.foregroundContext.arc(x,y,this.radius,0,Math.PI*2,false);
+			game.foregroundContext.fillStyle = game.selectionFillColor;
+			game.foregroundContext.fill();
+			game.foregroundContext.stroke();
+		},
+		draw:function(){
+			var x = (this.x*game.gridSize)-game.offsetX-this.pixelOffsetX;
+			var y = (this.y*game.gridSize)-game.offsetY-this.pixelOffsetY;
+			this.drawingX = x;
+			this.drawingY = y;
+			if (this.selected){
+				this.drawSelection();
+				this.drawLifeBar();
+			}
+			var colorIndex = (this.team == "blue")?0:1;
+			var colorOffset = colorIndex*this.pixelHeight;
+			game.foregroundContext.drawImage(this.spriteSheet,
+			this.imageOffset*this.pixelWidth,colorOffset,
+			this.pixelWidth,this.pixelHeight,x,y,this.pixelWidth,this.pixelHeight);
+		}
     },
     load:loadItem,
     add:addItem,
