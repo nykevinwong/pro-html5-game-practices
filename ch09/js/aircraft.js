da3bd378e53785a8d88f6a788e03010999f4d58f
nyckevinwong <nyckevinwong@gmail.com>
2019-02-20 22:38:56 -0500

    chapter 9 added attack mode

--- ch09/js/aircraft.js
+++ ch09/js/aircraft.js
@@ -70,18 +70,108 @@ var aircraft = {
 				this.y = (this.y +this.lastMovementY);
 			}
 		},
+		isValidTarget:isValidTarget,
+		findTargetsInSight:findTargetsInSight,
 		processOrders:function(){
 			this.lastMovementX = 0;
-		          this.lastMovementY = 0;
+			this.lastMovementY = 0;
+			if(this.reloadTimeLeft){
+				this.reloadTimeLeft--;
+			}
 			switch (this.orders.type){
-				case "move":
-					// Move towards destination and stop when close by
-					if ((Math.pow(this.orders.to.x-this.x,2) + Math.pow(this.orders.to.y-this.y,2))<Math.pow(this.radius/game.gridSize,2)) {
-						this.orders = {type:"float"};
-		               } else {
-		                  	this.moveTo(this.orders.to);					
-		               }												
+				case "float":
+					var targets = this.findTargetsInSight();
+					if(targets.length>0){
+						this.orders = {type:"attack",to:targets[0]};
+					}
 					break;
+				case "sentry":
+					var targets = this.findTargetsInSight(2);
+					if(targets.length>0){
+						this.orders = {type:"attack",to:targets[0],nextOrder:this.orders};
+					}
+					break;
+				case "hunt":
+					var targets = this.findTargetsInSight(100);
+					if(targets.length>0){
+						this.orders = {type:"attack",to:targets[0],nextOrder:this.orders};
+					}
+					break;
+				case "move":
+					// Move toward destination until distance from destination is less than aircraft radius
+					var distanceFromDestinationSquared = (Math.pow(this.orders.to.x-this.x,2) + Math.pow(this.orders.to.y-this.y,2));
+					if (distanceFromDestinationSquared < Math.pow(this.radius/game.gridSize,2)) {
+						this.orders = {type:"float"};
+					} else {
+						this.moveTo(this.orders.to);
+					}
+					break;
+				case "attack":
+					if(this.orders.to.lifeCode == "dead" || !this.isValidTarget(this.orders.to)){
+						if (this.orders.nextOrder){
+							this.orders = this.orders.nextOrder;
+						} else {
+							this.orders = {type:"float"};
+						}
+						return;
+					}
+					if ((Math.pow(this.orders.to.x-this.x,2) + Math.pow(this.orders.to.y-this.y,2))<Math.pow(this.sight,2)) {
+						//Turn toward target and then start attacking when within range of the target
+						var newDirection = findFiringAngle(this.orders.to,this,this.directions);
+						var difference = angleDiff(this.direction,newDirection,this.directions);
+						var turnAmount = this.turnSpeed*game.turnSpeedAdjustmentFactor;
+						if (Math.abs(difference)>turnAmount){
+							this.direction = wrapDirection(this.direction+ turnAmount*Math.abs(difference)/difference, this.directions);
+							return;
+						} else {
+							this.direction = newDirection;
+							if(!this.reloadTimeLeft){
+								this.reloadTimeLeft = bullets.list[this.weaponType].reloadTime;
+								var angleRadians = -(Math.round(this.direction)/this.directions)*2*Math.PI ; 
+								var bulletX = this.x- (this.radius*Math.sin(angleRadians)/game.gridSize);
+								var bulletY = this.y- (this.radius*Math.cos(angleRadians)/game.gridSize)-this.pixelShadowHeight/game.gridSize;
+								var bullet = game.add({name:this.weaponType, type:"bullets",x:bulletX, y:bulletY, direction:newDirection, target:this.orders.to});
+							}
+						}
+		  
+					} else {
+						var moving = this.moveTo(this.orders.to);
+					}
+					break;
+				case "patrol":
+					var targets = this.findTargetsInSight(1);
+					if(targets.length>0){
+						this.orders = {type:"attack",to:targets[0],nextOrder:this.orders};
+						return;
+					}
+					if ((Math.pow(this.orders.to.x-this.x,2) + Math.pow(this.orders.to.y-this.y,2))<Math.pow(this.radius/game.gridSize,2)) {
+						var to = this.orders.to;
+						this.orders.to = this.orders.from;
+						this.orders.from = to;
+					} else {
+						this.moveTo(this.orders.to);
+					}
+					break;
+				case "guard":
+					if(this.orders.to.lifeCode == "dead"){
+						if (this.orders.nextOrder){
+							this.orders = this.orders.nextOrder;
+						} else {
+							this.orders = {type:"float"};
+						}
+						return;
+					}
+					if ((Math.pow(this.orders.to.x-this.x,2) + Math.pow(this.orders.to.y-this.y,2))<Math.pow(this.sight-2,2)) {
+						var targets = this.findTargetsInSight(1);
+						if(targets.length>0){
+							this.orders = {type:"attack",to:targets[0],nextOrder:this.orders};
+							return;
+						}
+					} else {
+						this.moveTo(this.orders.to);
+					}
+					break;
+					 
 			}
 		},
 		animate:function(){
